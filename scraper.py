20c63c96c987772308f9350078cbd4f2c9e49a75
eeffiong-harv <eeffiong@college.harvard.edu>
2026-04-22 17:41:21 +0000

    Wed, Apr 22, 2026, 1:41 PM -04:00

--- scraper.py
+++ scraper.py
@@ -4,8 +4,9 @@ import datetime
 import pandas
 
 def main() :
-
-    
-
-
+    api_key = input("What is your API Key?")
+        # Checks if the key is valid
+    api_hash = input("What is your API Hash?")
+        # Checks if the hash is valid
+    print(api_key, api_hash)
     return
