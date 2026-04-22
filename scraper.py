84b4700f8d93e8b238b938278fdae11b1def4bed
eeffiong-harv <eeffiong@college.harvard.edu>
2026-04-22 17:44:43 +0000

    Wed, Apr 22, 2026, 1:44 PM -04:00

--- scraper.py
+++ scraper.py
@@ -8,7 +8,11 @@ def main() :
         # Checks if the key is valid
     api_hash = input("What is your API Hash?")
         # Checks if the hash is valid
+    channels = input("What channels would you like to scrape?")
+        # Loop of inputting channel name, checking for validity, and prompting for more
     print(api_key, api_hash)
+
+    print(f'Great! Attempting')
     return
 
 main()
